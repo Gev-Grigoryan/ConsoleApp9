abf5a26095a86dbfc545b74fa4466354130fb6d8
User <User@DESKTOP-N61M6PA>
2024-08-29 12:43:55 +0400

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Students students = new Students();
+            students.AddStudent(new Student("Arsen", 20, 85));
+            students.AddStudent(new Student("Artur", 22, 92));
+            students.AddStudent(new Student("Mane", 19, 95));
+            students.AddStudent(new Student("Mariam", 21, 88));
+            foreach (var student in students)
+            {
+                Console.WriteLine($"Name: {student.Name}, Age: {student.Age}, Grade: {student.Grade}");
+            }
+        }
+    }
+}
---
+++ ConsoleApp9/Student.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleApp9
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Grade { get; set; }  
+
+        public Student(string name, int age, int grade)
+        {
+            Name = name;
+            Age = age;
+            Grade = grade;
+        }
+    }
+
+    
+    public class Students : IEnumerable<Student>, IEnumerator<Student>
+    {
+        private List<Student> _students = new List<Student>();
+        private int position = -1;
+
+        public void AddStudent(Student student)
+        {
+            _students.Add(student);
+        }
+
+        public bool MoveNext()
+        {
+            while (++position < _students.Count)
+            {
+                if (_students[position].Grade > 90) 
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        public Student Current
+        {
+            get
+            {
+                return _students[position];
+            }
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        public void Dispose() { }
+
+
+        public IEnumerator<Student> GetEnumerator()
+        {
+            return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
